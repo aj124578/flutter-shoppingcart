2bd9e02b78d4ac6c823b4417acdd7f3c163c987f
ìaj124578 <aj124578@naver.com>
2023-03-27 17:50:16 +0900

    flutter-shoppingcart temp

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shoppingcart/components/shoppingcart_detail.dart';
+import 'package:flutter_shoppingcart/components/shoppingcart_header.dart';
+import 'package:flutter_shoppingcart/theme.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme(),
+      home: ShoppingCartPage(),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
+        ],
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          ShoppingCartHeader(),
+          ShoppingCartDetail(),
+        ],
+      ),
+    );
+  }
+}
+
---
+++ lib/size.dart
@@ -0,0 +1,6 @@
+// 앞에 const가 붙으면 double은 생략 가능
+const defalut_gap = 16.0;
+const s_gap = 5.0; // small gap
+const m_gap = 10.0; // medium gap
+const lg_gap = 20.0; // large gap
+const xlg_gap = 100.0; // xlarge gap
